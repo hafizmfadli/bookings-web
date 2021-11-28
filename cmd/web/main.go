6951050347d91e6e6067e058340908ef96a547a0
hafizmfadli <lshafizmf1st@gmail.com>
2021-11-28 16:57:08 +0700

    setup logger config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/hafizmfadli/bookings-web/internal/config"
 	"github.com/hafizmfadli/bookings-web/internal/handlers"
+	"github.com/hafizmfadli/bookings-web/internal/helpers"
 	"github.com/hafizmfadli/bookings-web/internal/models"
 	"github.com/hafizmfadli/bookings-web/internal/render"
 )
@@ -18,6 +20,8 @@ const portNumber = ":8080"
 
 var session *scs.SessionManager
 var app config.AppConfig
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 func main() {
 
@@ -45,6 +49,12 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	// set up the session
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
@@ -65,6 +75,7 @@ func run() error {
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
 	render.NewTemplate(&app)
+	helpers.NewHelpers(&app)
 
 	return nil
 }
