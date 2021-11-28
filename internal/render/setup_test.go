242897158f6449022b87eb0f891375962879a100
hafizmfadli <lshafizmf1st@gmail.com>
2021-11-28 07:12:14 +0700

    add test environtment for reander

---
+++ internal/render/setup_test.go
@@ -0,0 +1,36 @@
+package render
+
+import (
+	"encoding/gob"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/hafizmfadli/bookings-web/internal/config"
+	"github.com/hafizmfadli/bookings-web/internal/models"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+	gob.Register(models.Reservation{})
+
+	// change this to true when in production
+	testApp.InProduction = false
+
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+
+	app = &testApp
+
+	os.Exit(m.Run())
+}
